dad9c543c5c2e6fe140174cd99e8719219a468f1
jparrott06 <jparrott06@hotmail.com>
2021-02-28 13:03:21 -0700

    added error message divs to all input fields that do not already have a pattern attribute

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function formValidation() {
         divDoBError.classList.remove("invisible");
         divDoBError.innerHTML = "The Date of Birth cannot be empty."
         DoB.classList.add("hasError");
-        alert(DoB.value + " is empty string");
+        // alert(DoB.value + " is empty string");
         validForm = false;
     }
 
@@ -36,7 +36,7 @@ function formValidation() {
             divDoBError.classList.remove("invisible");
             divDoBError.innerHTML = "The Date of Birth must be before today's date."
             DoB.classList.add("hasError");
-            alert(DoB.value + " is passed today's date");
+            // alert(DoB.value + " is passed today's date");
             validForm = false;
         }
 
@@ -49,7 +49,48 @@ function formValidation() {
 
     //2) If user traveled, need to list at least one country
 
+    var ddTravel = document.querySelector("#ddTravel");
+    var divCountryError = document.querySelector("#divtxtCountryError");
+
+    if (ddTravel.value == "Yes") {
+        var txtCountry = document.querySelector("#txtCountry");
+        if (txtCountry.value == "") {
+            divCountryError.classList.remove("invisible");
+            divCountryError.innerHTML = "You must add at least one country if you have traveled to any country in the last 14 days."
+            txtCountry.classList.add("hasError");
+            validForm = false;
+        }
+
+        else {
+            divCountryError.classList.add("invisible");
+            txtCountry.classList.remove("hasError");
+        }
+    
+    }
+
     //3) There are no invalid chars (&, <, >, #, !, `, ", ~") in any field
+
+    var elements = document.getElementsByTagName("input");
+    var invalidChars = ['&', '<', '>', '#', '!', '`', '"', '~'];
+
+    for (let i = 0; i < elements.length; i++) {
+
+        var errorDiv = String("div" + elements[i].id + "Error");
+        var errorDivlocation = document.getElementById(errorDiv);
+
+        for (let j = 0; j < invalidChars.length; j++) {
+            if (elements[i].value.indexOf(invalidChars[j]) != -1) {
+                // alert(errorDivlocation.id);
+                errorDivlocation.classList.remove("invisible");
+                errorDivlocation.innerHTML = "This field contains an invalid character: &,<,>,#,!,`,\" or ~";
+                elements[i].classList.add("hasError");
+                validForm = false;
+
+            }
+            
+        }
+        
+    }
 
 
     //If any above validations fail - highlight the background of the element and show a corresponding message next to the box
